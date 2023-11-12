6fcd6c7590d59db9563d7bf988fb8ad9f31e0749
Santos <santoslukombo@gmail.com>
2023-11-12 15:39:39 +0100

    adding functions in main.rs

--- docker_image/solution/src/main.rs
+++ docker_image/solution/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+fn read_piece(piece_given: mut String)-> Vec<String>{
+    
+}
+
+fn get_grid_size(size_given: String)->(u32,u32){
+
+}
+
+
+
+
+
+
+
+
+
