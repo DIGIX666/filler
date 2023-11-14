bb15dbce8e1a7d36cc467243e51213acf8cdcb2a
théo dub <theodub@MacThox.local>
2023-11-14 13:52:46 +0100

    read player

--- docker_image/solution/src/main.rs
+++ docker_image/solution/src/main.rs
@@ -1,12 +1,19 @@
 use std::io;
 
-fn main() {
-    read_output();
+fn read_output() {
+    // Premier joueur
+    let mut player1_name = String::new();
+    io::stdin().read_line(&mut player1_name)
+        .expect("Failed to read line");
+    println!("Player 1: {}", player1_name.trim());
+
+    // Deuxième joueur
+    let mut player2_name = String::new();
+    io::stdin().read_line(&mut player2_name)
+        .expect("Failed to read line");
+    println!("Player 2: {}", player2_name.trim());
 }
 
-fn read_output() {
-    let mut playername = String::new();
-    io::stdin().read_line(&mut playername)
-        .expect("Failed to read line");
-        println!("{}", playername);
+fn main() {
+    read_output();
 }
