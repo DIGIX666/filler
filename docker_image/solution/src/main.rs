f24a5d5a730cec7ecf3d269024ac89418e110ba2
Santos <santoslukombo@gmail.com>
2023-11-14 21:23:28 +0100

    getting grid from game engine done

--- docker_image/solution/src/main.rs
+++ docker_image/solution/src/main.rs
@@ -2,11 +2,27 @@ mod piece;
 mod tools;
 
 use piece::read_piece;
-use tools::read_line;
+// use tools::read_line;
+use std::{
+    fs::File,
+    io::{self, stderr, stdout, BufRead, BufReader, BufWriter, Write},
+    process::Stdio,
+};
 
+use crate::tools::read_engine_output;
 
-fn main() {
+fn main() -> std::io::Result<()> {
 
-   println!("stdin: {}",read_line()) 
+    let mut file = File::create("output.txt").unwrap();
+    let mut reader = BufReader::new(io::stdin());
     
+    // writeln!(file, "== engine_output: {}", engine_output)?;
+    // let grid = tools::get_previous_grid_dirty(read_engine_output());
+    // reader.read_line(&mut grid).unwrap();
+    // writeln!(file, "== grid: {}", grid)?;
+
+    println!("4 3");
+    io::stdout().flush().unwrap();
+
+    Ok(())
 }
--- docker_image/solution/src/tools.rs
+++ docker_image/solution/src/tools.rs
@@ -1,64 +1,64 @@
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader,Write};
 
-pub fn read_line() -> String {
+pub fn read_engine_output() -> String {
     let mut input = String::new();
+    let mut player = String::new();
+    let mut engine_output = String::new();
+    let mut gz = String::new();
+    let mut length_engine_grid: u32 = 0;
+
+    let mut reader = BufReader::new(io::stdin());
 
     // Lire une ligne depuis l'entrée standard (terminal)
-    let stdin = io::stdin();
-    let reader = stdin.lock();
-    // let mut input = String::new();
 
-    // Itère sur chaque ligne
-    for line_result in reader.lines() {
-        match line_result {
-            Ok(line) => {
-                // Faites quelque chose avec la ligne lue
-                println!("Ligne lue : {}", line);
+    // let mut file = File::create("output.txt").unwrap();
 
-                for (_k, e) in line.chars().enumerate() {
-                        input.push(e);
-                    
-                }
-            }
-            Err(e) => {
-                eprintln!("Erreur lors de la lecture de la ligne : {}", e);
-                break;
-            }
-        }
+    reader.read_line(&mut player).unwrap();
+    // writeln!(file, "'{}'", player);
+
+    reader.read_line(&mut gz).unwrap();
+    // writeln!(file, "'{}'", gz);
+
+    let mut grid_size = String::new();
+    gz = gz.replace(":", "");
+    //  gz = gz.replace(" ", "");
+    gz = gz.replace("\n", "");
+    grid_size = gz.trim().to_string().split(" ").collect::<Vec<&str>>()[2].to_string();
+    // writeln!(file, "== grid_size: {}", grid_size);
+
+    length_engine_grid = grid_size.trim().parse::<u32>().unwrap();
+    // writeln!(file, "== length_engine_grid: {}", length_engine_grid);
+
+    for _i in 0..length_engine_grid {
+        let mut line = String::new();
+        reader.read_line(&mut line).unwrap();
+        // writeln!(file, "'{}'", line);
+        // writeln!(file, "line: {}", line.trim());
+        engine_output.push_str(&line);
     }
-    // Retirer le caractère de nouvelle ligne (\n) à la fin
-    return input;
+    reader.read_line(&mut engine_output).unwrap();
+    return engine_output;
 }
 
 
-pub fn get_previous_grid(std_input: String) -> Vec<String> {
-    let grid_raw = std_input.split(":").collect::<Vec<&str>>();
+pub fn get_previous_grid_dirty(std_input: String) -> String {
+   
 
-    let gr1 = grid_raw[1].replace("Anfield ", "");
-    let gr2 = gr1.replace(":", "");
-    let gr3 = gr2.replace(" ", "");
-    let gr4 = gr3.replace("\n", "");
-    let mut grid_dirty: Vec<String> = Vec::new();
+    let mut grid_dirty: String = String::new();
 
-    grid_dirty = gr4
-        .split("")
-        .collect::<Vec<&str>>()
-        .to_vec()
-        .iter()
-        .map(|x| x.to_string())
-        .collect::<Vec<String>>();
-
-    let mut grid_clean: Vec<String> = Vec::new();
-
-    for elmt in grid_dirty.iter() {
-        for (_k, e) in elmt.chars().enumerate() {
+    for e in std_input.chars() {
+        
             if !e.is_ascii_alphanumeric() {
-                grid_clean.push(e.to_string());
+                grid_dirty.push(e);
             }
-        }
+        
     }
 
-    return grid_clean;
+    return grid_dirty;
+}
+
+pub fn get_grid(){
+    
 }
 
