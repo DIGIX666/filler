36e0a4e2f95cf936536bfc846a4b3977b640d9ce
Santos <santoslukombo@gmail.com>
2023-11-14 14:06:11 +0100

    testing theo stding input

--- docker_image/solution/src/main.rs
+++ docker_image/solution/src/main.rs
@@ -2,16 +2,23 @@ use std::io;
 
 fn read_output() {
     // Premier joueur
-    let mut player1_name = String::new();
-    io::stdin().read_line(&mut player1_name)
-        .expect("Failed to read line");
-    println!("Player 1: {}", player1_name.trim());
+    let mut input = String::new();
+    let mut i = 0;
+    loop{
 
-    // Deuxième joueur
-    let mut player2_name = String::new();
-    io::stdin().read_line(&mut player2_name)
-        .expect("Failed to read line");
-    println!("Player 2: {}", player2_name.trim());
+        let mut player1_name = String::new();
+        io::stdin().read_line(&mut player1_name)
+            .expect("Failed to read line");
+        println!("Player 1: {}", player1_name);
+        input += &player1_name; 
+        i+=1;
+    }
+
+    // // Deuxième joueur
+    // let mut player2_name = String::new();
+    // io::stdin().read_line(&mut player2_name)
+    //     .expect("Failed to read line");
+    println!("input: {}", input);
 }
 
 fn main() {
