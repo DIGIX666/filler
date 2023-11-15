52c696f0fda47a6fd50a3aa013fa576daae6dade
théo dub <theodub@MacThox.local>
2023-11-15 11:34:56 +0100

    no change

--- docker_image/solution/src/main.rs
+++ docker_image/solution/src/main.rs
@@ -5,15 +5,18 @@ fn read_output() {
     let mut player1_name = String::new();
     io::stdin().read_line(&mut player1_name)
         .expect("Failed to read line");
-    println!("Player 1: {}", player1_name.trim());
+    println!("{}", player1_name.trim());
 
     // Deuxième joueur
     let mut player2_name = String::new();
     io::stdin().read_line(&mut player2_name)
         .expect("Failed to read line");
-    println!("Player 2: {}", player2_name.trim());
+    println!("{}", player2_name.trim());
 }
 
 fn main() {
     read_output();
 }
+
+
+1
