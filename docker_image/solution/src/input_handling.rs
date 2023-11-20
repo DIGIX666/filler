f884fab2e4bf4fb6973739f7d2bcffce3540d622
théo dub <theodub@MacThox.local>
2023-11-20 17:17:46 +0100

    place_piece

--- docker_image/solution/src/input_handling.rs
+++ docker_image/solution/src/input_handling.rs
@@ -1,39 +1,40 @@
 use std::io;
-use std::io::Write;
-use std::fs::OpenOptions;
+// use std::io::Write;
+// use std::fs::OpenOptions;
+use crate::place_piece::place_piece;
 
 /****************************************************\
-    Fonction pour traiter l'entrée.
+    Fonction pour traiter l'entrée et la sortie.
 \****************************************************/
 pub fn input_handling() {
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("output.txt")
-        .expect("Failed to open output.txt");
+    // let mut file = OpenOptions::new()
+    //     .create(true)
+    //     .append(true)
+    //     .open("output.txt")
+    //     .expect("Failed to open output.txt");
 
     let (play, enemy) = get_player_characters();
-    writeln!(file, "play: {:?}", play);
-    writeln!(file, "enemy: {:?}", enemy);
+    // writeln!(file, "play: {:?}", play);
+    // writeln!(file, "enemy: {:?}", enemy);
 
     // Boucle sans fin pour traiter l'entrée continuellement.
     loop {
         // Lecture de la grille.
-        let grid = read_grid(&mut file);
-        writeln!(file, "grid: {:?}", grid);
+        let grid = read_grid();
+        // writeln!(file, "grid: {:?}", grid);
 
         // Lecture de la pièce.
-        let piece = read_piece(&mut file);
-        writeln!(file, "piece: {:?}", piece);
+        let piece = read_piece();
+        // writeln!(file, "piece: {:?}", piece);
 
-       
+        // Appelle la fonction `place_piece` pour trouver la position idéale de la pièce.
+        let (piece_x, piece_y) = place_piece(&grid, &piece, &play, &enemy);
 
         // Affiche les coordonnées où la pièce doit être placée.
-        println!("{} {}", 0, 0);
+        println!("{} {}", piece_x, piece_y);
     }
 }
 //////////////////////// FIN //////////////////////////
-
 
 
 /****************************************************\
@@ -41,7 +42,9 @@ pub fn input_handling() {
 \****************************************************/
 fn get_player_characters() -> (Vec<char>, Vec<char>) {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read input");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
     let player_number = input.chars().nth(10).unwrap();
     if player_number == '1' {
         (vec!['@', 'a'], vec!['$', 's'])
@@ -49,26 +52,31 @@ fn get_player_characters() -> (Vec<char>, Vec<char>) {
         (vec!['$', 's'], vec!['@', 'a'])
     }
 }
-///////////////////////// FIN ////////////////////////////
-
+///////////////////////// FIN /////////////////////////
 
 
 /****************************************************\
     Fonction pour lire la grille à partir de l'entrée.
 \****************************************************/
-fn read_grid(file: &mut std::fs::File) -> Vec<Vec<char>> {
+fn read_grid() -> Vec<Vec<char>> {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read input");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
     let grid_details = input.split_whitespace().collect::<Vec<&str>>()[2];
-    let grid_lines = grid_details[..grid_details.len() - 1].parse::<i32>().unwrap();
-    
-    writeln!(file, "grid_details: {:?}", grid_details);
-    writeln!(file, "grid_line: {:?}", grid_lines);
+    let grid_lines = grid_details[..grid_details.len() - 1]
+        .parse::<i32>()
+        .unwrap();
+
+    // writeln!(file, "grid_details: {:?}", grid_details);
+    // writeln!(file, "grid_line: {:?}", grid_lines);
 
     let mut grid = Vec::new();
     for i in 0..grid_lines + 1 {
         input.clear();
-        io::stdin().read_line(&mut input).expect("Failed to read input");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
         if i < 1 {
             continue;
         } else {
@@ -85,25 +93,28 @@ fn read_grid(file: &mut std::fs::File) -> Vec<Vec<char>> {
 /****************************************************\
     Fonction pour lire la pièce à partir de l'entrée.
 \****************************************************/
-fn read_piece(file: &mut std::fs::File) -> Vec<Vec<char>> {
+fn read_piece() -> Vec<Vec<char>> {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read input");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
     let piece_details = input.split_whitespace().collect::<Vec<&str>>();
-    let piece_lines = piece_details[2][..piece_details[2].len() - 1].parse::<i32>().unwrap();
+    let piece_lines = piece_details[2][..piece_details[2].len() - 1]
+        .parse::<i32>()
+        .unwrap();
 
-    writeln!(file, "piece_lines: {:?}", piece_lines);
-    writeln!(file, "piece_details: {:?}", piece_details);
+    // writeln!(file, "piece_lines: {:?}", piece_lines);
+    // writeln!(file, "piece_details: {:?}", piece_details);
 
     let mut piece = Vec::new();
     for _ in 0..piece_lines {
         input.clear();
-        io::stdin().read_line(&mut input).expect("Failed to read input");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
         let row: Vec<char> = input[..input.len() - 1].chars().collect();
         piece.push(row);
     }
     piece
 }
 ///////////////////////// FIN /////////////////////////////
-
-
-
